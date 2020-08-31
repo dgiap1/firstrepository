c2d9cf82d5c22874dd854829d487f064f3b4455c
Derek Giap <dgiap1@student.cccd.edu>
2020-08-31 20:48:19 +0000

    auto commit 08/31/20 20:48:19

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -44,7 +44,7 @@ int run()
     int totalTime = timeHours * 60 + timeMinutes;
     int duration = durationHours * 60 + durationMinutes;
     int after = totalTime + duration;
-    int before = totalTime - duration + 1440;
+    int before = totalTime - duration;
     int afterHours = (((after / 60 % 12) + 11) % 12) + 1;
     int afterMinutes = after % 60;
     int beforeHours = before / 60;
