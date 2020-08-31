ecb322685f4ede26316deeeaed351f043c58a478
Derek Giap <dgiap1@student.cccd.edu>
2020-08-31 20:43:08 +0000

    auto commit 08/31/20 20:43:08

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -48,8 +48,10 @@ int run()
     int afterHours = (((after / 60 % 12) + 11) % 12) + 1;
     int afterMinutes = after % 60 * -1;
     int beforeHours = (((before / 60) + 11) % 12) + 1;
-    int beforeMinutes = (before % 60) * -1;
-
+    int beforeMinutes = (before % 60);
+        if (beforeMinutes < 0) {
+            beforeMinutes = beforeMinutes * 1;
+        }
     //Output
     cout << "\n";
     cout << setfill('0');
