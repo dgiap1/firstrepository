29f815f66ce15f0b9ad94bb59134484855dc5c18
Derek Giap <dgiap1@student.cccd.edu>
2020-08-31 20:14:35 +0000

    auto commit 08/31/20 20:14:34

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 08/31/2020
  *  @file h01.cpp
  */
 #include <iostream>
@@ -8,7 +8,7 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "dgiap1";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 
@@ -18,9 +18,43 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    // Add your implementation comments here
+    // 1. Title and introduction
+    // - blank line
+    // 2. Input section: prompt and input on same line
+    // 3. Processing section - compute the results
+    // - blank line
+    // 4. Output section: test data inside brackets [ ]
 
-    // Write your code here
+    // Prompt and read the input
+    cout << "   Time: ";
+    int timeHours;
+    cin >> timeHours;
+    cin.get(); // discard the colon
+    int timeMinutes;
+    cin >> timeMinutes;
+
+    cout << "   Duration: ";
+    int durationHours;
+    cin >> durationHours;
+    cin.get(); // discard next
+    int durationMinutes;
+    cin >> durationMinutes;
+
+    //Processing
+    int sumTime = timeHours * 60 + timeMinutes;
+    int duration = durationHours * 60 + durationMinutes;
+    int after = sumTime + duration;
+    int before = sumTime - duration;
+    int afterHours = after / 60;
+    int afterMinutes = after % 60;
+    int beforeHours = before / 60;
+    int beforeMinutes = before % 60;
+    
+    //Output
+    cout << endl;
+    cout << durationHours << ":" << durationMinutes;
+    cout << afterHours << ":" << afterMinutes;
+    
 
     return 0;
 }
