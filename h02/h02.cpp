7efc38a8160d56924f320148b434151af7fcbecd
Derek Giap <dgiap1@student.cccd.edu>
2020-08-31 21:08:03 +0000

    auto commit 08/31/20 21:08:03

--- h02/h02.cpp
+++ h02/h02.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 08/31/2020
  *  @file h02.cpp
  */
 #include <iostream>
@@ -8,7 +8,7 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "dgiap1";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 /**
@@ -19,7 +19,29 @@ int run()
 {
     // Add your implementation comments here
 
-    // Write your code here
+    const double SODA_CAN_WEIGHT = 350;
+    const double GRAMS_PER_POUND = 454;
+    const double SWEETENER = 0.001;
+    const double SWEETENER_PER_CAN = 0.35;
+    int mouseWeight;
+    int lethalDoseMouse;
+    int desiredWeightPounds;
+    
+    //Input
+    cout << "Weight of the mouse in grams: ";
+    cin >> mouseWeight;
+    cout << "Lethal dose for the mouse (in grams): ";
+    cin >> lethalDoseMouse;
+    cout << "Desired weight of the dieter (in pounds): ";
+    cin >> desiredWeightPounds;
+
+    //Processing
+    double desiredWeightGrams = desiredWeightPounds * GRAMS_PER_POUND;
+    double lethalDoseDieter = lethalDoseMouse * (desiredWeightGrams / mouseWeight);
+    double lethalDoseDieterCans = lethalDoseDieter / SWEETENER_PER_CAN;
+
+    //Output
+    cout << "Lethal dose in grams, cans is [" << lethalDoseDieter << ", " << lethalDoseDieterCans << "]" << endl;
 
     return 0;
 }
