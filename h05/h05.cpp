45f4a38936adbdb7ea252cc6727fa05fef246a15
Derek Giap <dgiap1@student.cccd.edu>
2020-09-10 22:02:43 +0000

    auto commit 09/10/20 22:02:43

--- h05/h05.cpp
+++ h05/h05.cpp
@@ -1,20 +1,22 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 09/10/2020
  *  @file h05.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "dgiap1";  // Add your Canvas login name
 
-// Write your function here
+string toFrenchGender(const string& country)
+{
+    string result = "who knows?";
+    return result;  
+}
 
 
 /////////////// Optional Student Code /////////////////
 int run()
 {
-    // You may add any code you like
-    // It will be displayed with make run
     return 0;
 }
