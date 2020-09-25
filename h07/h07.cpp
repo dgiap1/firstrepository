54f62729d6439e75ac6944dd739c6cd0833012c1
Derek Giap <dgiap1@student.cccd.edu>
2020-09-25 00:03:07 +0000

    commiting updated starters

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -1,13 +1,13 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 09/14/2020
  *  @file h07.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h07.h"
 
--- h08/h08.cpp
+++ h08/h08.cpp
@@ -1,18 +1,30 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 09/24/2020
  *  @file h09.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h08.h"
 
 // Put your function implementation (definitions) in this file
+string codeForDigit(int digit)
+{
 
+}
 
+int checkDigit(int zip)
+{
+    
+}
+
+string barCode(int zip)
+{
+    
+}
 
 
 
