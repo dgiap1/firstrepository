5aa96b9e7d2c15b8eb9a916934eeb6e12227d876
Derek Giap <dgiap1@student.cccd.edu>
2020-10-01 00:11:27 +0000

    auto commit 10/01/20 00:11:27

--- h11/h11.cpp
+++ h11/h11.cpp
@@ -12,12 +12,46 @@ string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 // Write your function here
 void strip()
 {
+    bool inSingleCmt = false;
+    bool inMultiCmt = false;
+    bool inString = false;
+    char ch;
+    while(cin.get(ch))
+    {
+        if (inSingleCmt && ch == '\n')
+        {
+            inSingleCmt = false;
+        }
+        else if (inString && ch == '"')
+        {
+            inString = false;
+        }
+        else if (inMultiCmt && ch == '*' && cin.peek() == '/')
+        {
+            cin.get(ch);
+        }
+        else if (!inString && !inSingleCmt && !inMultiCmt)
+        {
+            if (ch == '"')
+            {
+                inString = true;
+            }
+            else if (ch == '/' && cin.peek() == '*')
+            {
+                inMultiCmt = true;
+            }
+            else if (ch == '/' && cin.peek() == '/')
+            {
+                inSingleCmt = true;
+            }
+        }
+        cout.put(ch);
+    }
 }
 
 //////////////// STUDENT TESTING ////////////////////
 int run()
 {
     //cout << "Student testing" << endl;
-    strip();
     return 0;
 }
