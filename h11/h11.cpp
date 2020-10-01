39ca7412fc2d292bc9291df9a80dcd1b6ce91709
Derek Giap <dgiap1@student.cccd.edu>
2020-10-01 00:05:17 +0000

    auto commit 10/01/20 00:05:17

--- h11/h11.cpp
+++ h11/h11.cpp
@@ -1,16 +1,18 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 09/28/2020
  *  @file h11.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 // Write your function here
-
+void strip()
+{
+}
 
 //////////////// STUDENT TESTING ////////////////////
 int run()
