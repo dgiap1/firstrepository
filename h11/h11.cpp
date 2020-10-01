4138a0cc16fafe32b35ef79d0ace17c608065c83
Derek Giap <dgiap1@student.cccd.edu>
2020-10-01 00:23:16 +0000

    auto commit 10/01/20 00:23:16

--- h11/h11.cpp
+++ h11/h11.cpp
@@ -30,6 +30,7 @@ void strip()
         {
             inMultiCmt = false;
             cin.get(ch);
+            cin.get(ch);
         }
         else if (inString && ch == '\\' && cin.peek() == '"')
         {
