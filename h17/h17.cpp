1cbdd3cda67742d0fb557b392e122d8edd653ddd
Derek Giap <dgiap1@student.cccd.edu>
2020-10-13 20:48:37 +0000

    auto commit 10/13/20 20:48:37

--- h17/h17.cpp
+++ h17/h17.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/13/2020
  *  @file h17.cpp
  */
 #include <iostream>
@@ -11,15 +11,28 @@
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 
 #include "h17.h"
 
 // Implement the function here
-
-
-
+vector<string> fileToWords(const string& filename)
+{
+    ifstream in(filename.c_str());
+    if(! in.is_open())
+    {
+        throw invalid_argument("Filename is Invalid.");
+    }
+    vector<string> results;
+    string line;
+    while (! in.eof())
+    {
+        getline(in, line);
+        results.push_back(line);
+    }
+    return results;
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
