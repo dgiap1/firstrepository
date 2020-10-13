432e7c24cb8b999f7962a4fd72a5051fb09fb06b
Derek Giap <dgiap1@student.cccd.edu>
2020-10-13 21:23:49 +0000

    auto commit 10/13/20 21:23:49

--- h18/h18.cpp
+++ h18/h18.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/13/2020
  *  @file h18.cpp
  */
 #include <iostream>
@@ -11,12 +11,77 @@
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h18.h"
 
 // Place your function here
 
+string clean(string& str)
+{
+    size_t i = 0;
+    size_t len = str.length();
+    while (i < len)
+    {
+        if (! isalpha(str[i]) || (! isdigit(str[i])) || str[i] == ' ')
+        {
+            str.erase(i, 1);
+            len--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return str;
+}
+
+vector<Word> spellCheck(istream& in, const vector<string>& dictionary, const vector<string>& excluded)
+{
+    vector<Word> results;
+    while(in)
+    {
+        pos_type currentPosition = in.tellg();
+        if(currentPosition == -1)
+        {
+            break;
+        }
+        string tempWord;
+        vector<pos_type> wordPos;
+        in >> tempWord >> ws;
+        tempWord = clean(tempWord);
+        for (auto& e : results)
+        {
+            if (tempWord == e.word)
+            {
+                e.positions.push_back(currentPosition);
+                continue;
+            }
+        }
+
+        for (auto& e : excluded)
+        {
+            if (tempWord == e)
+            {
+                continue;
+            }
+        }
+
+        for (auto& e : dictionary)
+        {
+            if (tempWord == e)
+            {
+                continue;
+            }
+            else
+            {
+                Word misspelled = {tempWord, wordPos};
+                results.push_back(misspelled);
+            }
+        }
+    }
+    return results;
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
