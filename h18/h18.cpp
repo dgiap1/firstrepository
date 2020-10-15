54d35ddbc3ea7b44c38199d6755e4356f6a88471
Derek Giap <dgiap1@student.cccd.edu>
2020-10-15 20:16:13 +0000

    auto commit 10/15/20 20:16:13

--- h18/h18.cpp
+++ h18/h18.cpp
@@ -26,6 +26,7 @@ vector<Word> spellCheck(istream& in, const vector<string>& dictionary, const vec
         string tempWord, newWord;
         vector<pos_type> wordPos;
         in >> tempWord >> ws;
+
         for (char ch : tempWord)
         {
             if (isalpha(ch) || isdigit(ch)) {newWord += tolower(ch);}
@@ -68,7 +69,6 @@ vector<Word> spellCheck(istream& in, const vector<string>& dictionary, const vec
     return results;
 }
 
-
 /////////////// STUDENT TESTING ////////////////////
 vector<string> fileToWords(const string& filename);
 #include <sstream>
@@ -76,20 +76,20 @@ vector<string> fileToWords(const string& filename);
 int run()
 {
     cout << "Student testing" << endl;
-    // vector<string> dictionary = fileToWords("words");
-    // vector<string> ignore = fileToWords("excluded.txt");
-    // istringstream words("Now is the tyme for all good studunts to "
-    //     "come to the aiid of their ai!id classmaates.");
-    // vector<Word> misspelled = spellCheck(words, dictionary, ignore);
-    // cout << "Misspelled words" << endl;
-    // int i{1};
-    // for (const auto& e : misspelled)
-    // {
-    //     cout << setw(4) << i++ << ". " << e.word << ", [ ";
-    //     for (auto pos : e.positions)
-    //         cout << pos << " ";
-    //     cout << "]" << endl;
-    // }
+    vector<string> dictionary = fileToWords("words");
+    vector<string> ignore = fileToWords("excluded.txt");
+    istringstream words("Now is the tyme for all good studunts to "
+        "come to the aiid of their ai!id classmaates.");
+    vector<Word> misspelled = spellCheck(words, dictionary, ignore);
+    cout << "Misspelled words" << endl;
+    int i{1};
+    for (const auto& e : misspelled)
+    {
+        cout << setw(4) << i++ << ". " << e.word << ", [ ";
+        for (auto pos : e.positions)
+            cout << pos << " ";
+        cout << "]" << endl;
+    }
 
     return 0;
 }
