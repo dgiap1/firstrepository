85fd7559d3930aa91638da8f2897eea67af7e304
Derek Giap <dgiap1@student.cccd.edu>
2020-10-22 21:09:52 +0000

    auto commit 10/22/20 21:09:52

--- h20/h20.cpp
+++ h20/h20.cpp
@@ -1,18 +1,37 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/21/2020
  *  @file h20.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h20.h"
 
 // Add your code here
-
+void greenScreen(unsigned char* const img, int width, int height)
+{
+    auto end = img + width * height * 4;
+    unsigned char * p = img;
+    while (p != end)
+    {
+        auto bigger = max(*p, *(p+2));
+        if(*(p+1) >= 2 * bigger)
+        {
+            *p = 0;
+            *(p + 1) = 0;
+            *(p + 2) = 0;
+            *(p + 3) = 0;
+        }
+        p++;
+        p++;
+        p++;
+        p++;
+    }
+}
 
 
 
