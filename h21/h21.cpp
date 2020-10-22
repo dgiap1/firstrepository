db52f9d91ef0ede86d9409164d25408cb191028e
Derek Giap <dgiap1@student.cccd.edu>
2020-10-22 21:12:45 +0000

    auto commit 10/22/20 21:12:45

--- h21/h21.cpp
+++ h21/h21.cpp
@@ -1,18 +1,36 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/21/2020
  *  @file h21.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h21.h"
 
 // Add your code here
-
+void composite(unsigned char * const bg, unsigned char * const fg, int width, int height)
+{
+    greenScreen(fg, width, height);
+    unsigned char *dest = fg;
+    unsigned char *src = bg;
+    unsigned char *end = fg + width * height * 4;
+    while (dest != end)
+    {
+        if (*dest == 0 && *(dest + 1) == 0 && *(dest + 2) == 0 && *(dest + 3) == 0)
+        {
+            *dest = *src;
+            *(dest + 1) = *(src + 1);
+            *(dest + 2) = *(src + 2);
+            *(dest + 3) = *(src + 3);
+        }
+        src += 4;
+        dest += 4;
+    }
+}
 
 
 
