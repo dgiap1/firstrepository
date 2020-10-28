8f75ffd42a9b3a3f50ec26e1b0b4455ad57e1d89
Derek Giap <dgiap1@student.cccd.edu>
2020-10-28 00:43:28 +0000

    auto commit 10/28/20 00:43:26

--- h22/h22.cpp
+++ h22/h22.cpp
@@ -1,17 +1,39 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/27/2020
  *  @file h22.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h22.h"
 
 // Add your code here
+void flip(UC* const img, int width, int height, Direction dir)
+{
+    for (int row = 0; row < height; ++row)
+    {
+        Pixel * front = reinterpret_cast<Pixel*>(img) + row * width * 4;
+        Pixel * back = front + width - 1;
+        Pixel * temp{};
+        while (front < back)
+        {
+            *temp = *front;
+            *front = *back;
+            *back = *temp;
+            front++;
+            back--;
+        }
+    }
+}
+
+void mirror(UC* const img, int width, int height, Direction dir)
+{
+
+}
 
 
 
