fc1724a2d48584196becf1b119b080c979c83e30
Derek Giap <dgiap1@student.cccd.edu>
2020-10-28 18:21:37 +0000

    auto commit 10/28/20 18:21:36

--- h22/h22.cpp
+++ h22/h22.cpp
@@ -28,11 +28,56 @@ void flip(UC* const img, int width, int height, Direction dir)
             back--;
         }
     }
+    for (int col = 0; col < width; ++col)
+    {
+        Pixel * top = reinterpret_cast<Pixel*>(img) + col * height * 4;
+        Pixel * bottom = top + width * (height - 1);
+        Pixel * temp2{};
+        while (top < bottom)
+        {
+            *temp2 = *top;
+            *top = *bottom;
+            *bottom = *temp2;
+            top = top + width;
+            bottom = bottom - width;
+        }
+    }
 }
 
 void mirror(UC* const img, int width, int height, Direction dir)
 {
-
+    for (int row = 0; row < height; ++row)
+    {
+        Pixel * front = reinterpret_cast<Pixel*>(img) + row * width * 4;
+        Pixel * back = front + width - 1;
+        while (front < back)
+        {
+            if (dir == Direction::LtoR)
+            {
+                *back = *front;
+            }
+            if (dir == Direction::RtoL)
+            {
+                *front = *back;
+            }
+        }
+    }
+    for (int col = 0; col < width; ++col)
+    {
+        Pixel * top = reinterpret_cast<Pixel*>(img) + col * height * 4;
+        Pixel * bottom = top + width * (height - 1);
+        while (top < bottom)
+        {
+            if (dir == Direction::TtoB)
+            {
+                *top = *bottom;
+            }
+            if (dir == Direction::BtoT)
+            {
+                *bottom = *top;
+            }
+        }
+    }
 }
 
 
