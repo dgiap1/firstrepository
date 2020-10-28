260755250d1e0af74566de506cf5fa309aa44abe
Derek Giap <dgiap1@student.cccd.edu>
2020-10-28 19:27:36 +0000

    auto commit 10/28/20 19:27:36

--- h24/h24.cpp
+++ h24/h24.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Derek Giap
+ *  @date 10/27/2020
  *  @file h24.cpp
  */
 #include <string>
@@ -8,13 +8,23 @@
 // Other headers if necessary
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h24.h"
 
-// Add your code here
+bool sameSet(const int *aBeg, const int *aEnd, const int *bBeg, const int *bEnd)
+{
+    bool result = true;
+    for (size_t i = 0; i < (unsigned int)*aEnd; i++)
+    {
+        result = false;
+    }
+    return result;
+}
 
-
+void copyEvens(const int a[], size_t aSize, int b[], size_t& bSize)
+{
+}
 
 /////////////// STUDENT TESTING ////////////////////
 #include <iostream>
