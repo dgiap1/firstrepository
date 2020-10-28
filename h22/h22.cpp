4b2020eeb2d5a5af6787d0015df13e39fbeee8e2
Derek Giap <dgiap1@student.cccd.edu>
2020-10-28 19:06:40 +0000

    auto commit 10/28/20 19:06:40

--- h22/h22.cpp
+++ h22/h22.cpp
@@ -17,7 +17,7 @@ void flip(UC* const img, int width, int height, Direction dir)
     {
         for (int row = 0; row < height - 1; ++row)
         {
-            Pixel * front = reinterpret_cast<Pixel*>(img) + row * width * 4;
+            Pixel * front = reinterpret_cast<Pixel*>(img) + row * width;
             Pixel * back = front + width - 1;
             while (front < back)
             {
@@ -34,7 +34,7 @@ void flip(UC* const img, int width, int height, Direction dir)
     {
         for (int col = 0; col < width - 1; ++col)
         {
-            Pixel * top = reinterpret_cast<Pixel*>(img) + col * height * 4;
+            Pixel * top = reinterpret_cast<Pixel*>(img) + col;
             Pixel * bottom = top + width * (height - 1);
             while (top < bottom)
             {
