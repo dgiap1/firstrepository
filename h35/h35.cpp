d143d34143ca2c6309906a534e6fdeaa286d9fe2
Derek Giap <dgiap1@student.cccd.edu>
2020-11-18 23:08:49 +0000

    auto commit 11/18/20 23:08:49

--- h35/h35.cpp
+++ h35/h35.cpp
@@ -1,7 +1,7 @@
 /**
     @file h35.cpp
-    @author your name here
-    @version what day and meeting time
+    @author Derek Giap
+    @version Fall2020TTEVE
 */
 #include <string>
 #include <stdexcept>
@@ -11,12 +11,51 @@ using namespace std;
 
 #include "h35.h"
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 // Add your implementation here
+Worker::Worker(const string& name, double rate)
+{
+    this -> name = name;
+    this -> rate = rate;
+}
+double Worker::getRate() const
+{
+    return rate;
+}
+string Worker::getName() const
+{
+    return name;
+}
 
+HourlyWorker::HourlyWorker(const string& NAME, double RATE):Worker(NAME, RATE) {}
 
+string HourlyWorker::payInfo(int hours) const
+{
+    double pay = 0.0;
+    if (hours <= 40)
+    {
+        pay = hours * getRate();
+    }
+    else
+    {
+        pay = hours * getRate() + (hours - 40) * getRate() / 2;
+    }
+    ostringstream os;
+    os << getName() << "(Hourly, $ " << fixed << setprecision(2) << getRate() << ") worked " <<
+    hours << " hours. Pay: $ " << fixed << setprecision(2) << pay;
+    return os.str();
+}
 
+SalariedWorker::SalariedWorker(const string& NAME, double RATE):Worker(NAME, RATE) {}
+
+string SalariedWorker::payInfo(int hours) const
+{
+    ostringstream os;
+    os << getName() << "(Salaried, $ " << fixed << setprecision(2) << getRate() << ") worked " <<
+    hours << " hours. Pay: $ " << fixed << setprecision(2) << 40 * getRate();
+    return os.str();
+}
 
 /////////////// Student Tests ////////////////////////////
 #include <iostream>
