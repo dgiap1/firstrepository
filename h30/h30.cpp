c644198a44cc4f8674d4b9d66227e2a6a1a0716e
Derek Giap <dgiap1@student.cccd.edu>
2020-11-18 22:36:58 +0000

    auto commit 11/18/20 22:36:57

--- h30/h30.cpp
+++ h30/h30.cpp
@@ -1,20 +1,140 @@
 /**
  *  @file h30.cpp
- *  @author Put your name here
- *  @version Put the semester and meeting time here
+ *  @author Derek Giap
+ *  @version Fall2020TTEVE
  */
 #include <string>
 #include <stdexcept>
+#include <sstream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h30.h"
 
-// Implement your class members here
+static int gcd(int x, int y)
+{
+    int r = x % y;
+    while (r != 0)
+    {
+        x = y;
+        y = r;
+        r = x % y;
+    }
+    return y;
+}
 
+Fraction::Fraction(int n)
+{
+    numerator_ = n;
+    denominator_ = 1;
+}
 
+Fraction::Fraction(int n, int d)
+{
+    numerator_ = n;
+    denominator_ = d;
+    if (d == 0)
+    {
+        cout << "Invalid arguments.";
+    }
+    else if (n == 0)
+    {
+        numerator_ = 0;
+        denominator_ = 1;
+    }
+    else
+    {
+        int g = gcd(abs(n), abs(d));
+        numerator_ = n/g;
+        denominator_ = abs(d)/g;
+        if (d < 0)
+        {
+            numerator_ = -numerator_;
+        }
+    }
+}
 
+Fraction& Fraction::operator+=(const Fraction& rhs)
+{
+    int a = numerator_;
+    int b = denominator_;
+    int c = rhs.numerator_;
+    int d = rhs.denominator_;
+    *this = Fraction(a * d + b * c, b * d);
+    return *this;
+}
+
+Fraction& Fraction::operator-=(const Fraction& rhs)
+{
+    int a = numerator_;
+    int b = denominator_;
+    int c = rhs.numerator_;
+    int d = rhs.denominator_;
+    *this = Fraction(a * d - b * c, b * d);
+    return *this;
+}
+
+Fraction& Fraction::operator*=(const Fraction& rhs)
+{
+    int a = numerator_;
+    int b = denominator_;
+    int c = rhs.numerator_;
+    int d = rhs.denominator_;
+    *this = Fraction(a * c, b * d);
+    return *this;
+}
+
+Fraction& Fraction::operator/=(const Fraction& rhs)
+{
+    int a = numerator_;
+    int b = denominator_;
+    int c = rhs.numerator_;
+    int d = rhs.denominator_;
+    *this = Fraction(a * d, b * c);
+    return *this;
+}
+
+const Fraction operator+(const Fraction& lhs, const Fraction& rhs)
+{
+    Fraction result(lhs);
+    return result += rhs;
+}
+
+const Fraction operator-(const Fraction& lhs, const Fraction& rhs)
+{
+    Fraction result(lhs);
+    return result -= rhs;
+}
+
+const Fraction operator*(const Fraction& lhs, const Fraction& rhs)
+{
+    Fraction result(lhs);
+    return result *= rhs;
+}
+
+const Fraction operator/(const Fraction& lhs, const Fraction& rhs)
+{
+    Fraction result(lhs);
+    return result /= rhs;
+}
+
+string Fraction::toString() const
+{
+    ostringstream out;
+    out << to_string(numerator_);
+    if(denominator_ != 1)
+    {
+        out << "/" << to_string(denominator_);
+    }
+    return out.str();
+}
+
+ostream& operator<<(ostream& out, const Fraction& f)
+{
+    out << f.toString();
+    return out;
+}
 
 /////////////// STUDENT TESTING ////////////////////
 
