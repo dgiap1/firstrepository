eb2fe2e2acc899f96a09a94cf09987dbc1277c71
Derek Giap <dgiap1@student.cccd.edu>
2020-11-18 22:58:46 +0000

    auto commit 11/18/20 22:58:46

--- h34/h34.cpp
+++ h34/h34.cpp
@@ -1,7 +1,7 @@
 /**
     @file h34.cpp
-    @author your name here
-    @version what day and meeting time
+    @author Derek Giap
+    @version Fall2020TTEVE
 */
 #include <string>
 #include <stdexcept>
@@ -9,16 +9,62 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "dgiap1"; // Add your Canvas/occ-email ID
 
 #include "h34.h"
 ///////// Add your code here ///////////////
+Circle::Circle(double r, double x, double y) : Point(x, y)
+{
+    radius = r;
+}
+double Circle::getRadius() const
+{
+    return radius;
+}
 
+double Circle::getArea() const
+{
+    return PI * radius * radius;
+}
 
+double Circle::getCircumference() const
+{
+    return 2 * PI * radius;
+}
 
+string Circle::toString(int decimals) const
+{
+    ostringstream os;
+    os << fixed << setprecision(decimals) << radius;
+    return "Circle(radius=" + os.str() + ", center=" + Point::toString(decimals) + ")";
+}
 
+Cylinder::Cylinder(double h, double r, double x, double y) : Circle(r, x, y)
+{
+    height = h;
+}
 
+double Cylinder::getHeight() const
+{
+    return height;
+}
 
+double Cylinder::getVolume() const
+{
+    return Circle::getArea() * Cylinder::getHeight();
+}
+
+double Cylinder::getArea() const
+{
+    return Circle::getCircumference() * Cylinder::getHeight() + 2 * Circle::getArea();
+}
+
+string Cylinder::toString(int decimals) const
+{
+    ostringstream os;
+    os << fixed << setprecision(decimals) << height;
+    return "Cylinder(height=" + os.str() + ", base=" + Circle::toString(decimals) + ")";
+}
 
 //////////////// Student Tests ////////////////////////////
 #include <iostream>
